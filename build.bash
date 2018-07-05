1ede0b8c5a113e64504966f4ad9e3f7619432d91
Chuning <cw686@cwang.marine.rutgers.edu>
2018-07-05 15:00:36 -0400

    first revamp finished.

--- build.bash
+++ build.bash
@@ -3,8 +3,9 @@ rm *.o *.mod *.exe
 gfortran -c mod_kinds.f90
 gfortran -c mod_iceplume_py.f90
 gfortran -c mod_iceplume.f90
-gfortran -c opkd.f90
-gfortran -c iceplume_model.f90
+gfortran -c iceplume_opkd.f90
+gfortran -c iceplume_core.f90
+gfortran -c iceplume_calc.f90
 gfortran -c iceplume.f90
 
 gfortran *.o -o iceplume_test.exe
